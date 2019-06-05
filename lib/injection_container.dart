8805f3828c1049bfadafc70906caf992013ffa2c
DanielLopez <d.lopez.torres@accenture.com>
2019-06-05 17:55:09 +0200

    [ADD] detail UI

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,6 +2,7 @@ import 'package:kiwi/kiwi.dart' as kiwi;
 import 'package:http/http.dart' as http;
 import 'package:youtube_search/network/youtube_data_source.dart';
 import 'package:youtube_search/repository/YoutubeRepository.dart';
+import 'package:youtube_search/ui/detail/detail_bloc.dart';
 import 'package:youtube_search/ui/search/search_bloc.dart';
 
 void initKiwi() {
@@ -9,5 +10,6 @@ void initKiwi() {
     ..registerInstance(http.Client())
     ..registerFactory((factory) => YoutubeDataSource(factory.resolve()))
     ..registerFactory((factory) => YoutubeRepository(factory.resolve()))
-    ..registerFactory((factory) => SearchBloc(factory.resolve()));
+    ..registerFactory((factory) => SearchBloc(factory.resolve()))
+    ..registerFactory((c) => DetailBloc(c.resolve()));
 }
